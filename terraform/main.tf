073748abdaf79cc2dadac0d2a22b571b9979703e
Eugene Ermakov <eugeniy696@gmail.com>
2026-08-19 23:22:10 +0300

    Solution

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  storage_container_name = azurerm_storage_container.example.name
+  storage_account_name   = azurerm_storage_account.example.name
+  name                   = var.storage_blob_name
+  type                   = "Block"
+  source                 = "terraform.tar.gz"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type    = string
+  default = "example-resources"
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "examplestorageacc696"
+}
+
+variable "storage_container_name" {
+  type    = string
+  default = "example-container"
+}
+
+variable "storage_blob_name" {
+  type    = string
+  default = "my-blob.txt"
+}
